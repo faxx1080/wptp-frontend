9539211923c126133a8a89dbf88f720f4901b0f9
Yuxiang Dong <yuxiang.dong@stonybrook.edu>
2023-12-14 00:27:30 -0500

    fixed date confusion.

--- src/data/test.js
+++ src/data/test.js
@@ -1,14 +1,15 @@
+// Keep in mind that JavaScript months are 0-based, so January is 0 and December is 11.
 const sampleTest = [
     {
         "id": 1,
-        "date": new Date(2023, 10, 31),
+        "date": new Date(2023, 10, 30),
         "reading": 300,
         "writing": 300,
         "math": 500,
     },
     {
         "id": 2,
-        "date": new Date(2023, 12, 1),
+        "date": new Date(2023, 11, 1),
         "reading": 390,
         "writing": 390,
         "math": 690,
