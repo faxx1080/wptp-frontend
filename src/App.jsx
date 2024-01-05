47def66c1f1002c0a7d746cfe6e36fe6a051a1c0
Yuxiang Dong <yuxiang.dong@stonybrook.edu>
2024-01-04 19:05:24 -0500

    Added a default login and a signup page.

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,19 @@ import TestResult from "./Pages/TestResult";
 import TestResultDetail from "./Pages/TestResultDetail";
 import Login from "./Pages/Login";
 import Welcome from "./Pages/Welcome";
+import Register from "./Pages/Register";
 import { Toolbar } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <TopNavbar />
-      <Toolbar />{" "}
+      <Toolbar />
       {/* This is to push the content down so that it is not hidden by the navbar */}
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
         <Route path="/Welcome" element={<Welcome />} />
         <Route path="/SAT" element={<SAT />} />
         <Route path="/ViewRandomQuestion" element={<ViewRandomQuestion />} />
