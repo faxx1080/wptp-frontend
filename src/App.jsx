a0f643ab0269b93f12a4da3516e2e1ff8ad0fafc
Yuxiang Dong <yuxiang.dong@stonybrook.edu>
2024-01-06 20:59:36 -0500

    Add a new home page and replaced the old home page with the dashboard page.

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import ViewRandomQuestion from "./Pages/ViewRandomQuestion";
 import TestResult from "./Pages/TestResult";
 import TestResultDetail from "./Pages/TestResultDetail";
 import Login from "./Pages/Login";
-import Welcome from "./Pages/Welcome";
 import Register from "./Pages/Register";
+import Dashboard from "./Pages/Dashboard";
 import { Toolbar } from "@mui/material";
 
 function App() {
@@ -19,9 +19,9 @@ function App() {
       {/* This is to push the content down so that it is not hidden by the navbar */}
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/Register" element={<Register />} />
-        <Route path="/Welcome" element={<Welcome />} />
         <Route path="/SAT" element={<SAT />} />
         <Route path="/ViewRandomQuestion" element={<ViewRandomQuestion />} />
         <Route path="/TestResult/:id" element={<TestResultDetail />} />
